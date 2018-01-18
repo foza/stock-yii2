b5867d6dff30f760596091c6f26be30830126191
foza <webfoza@gmail.com>
2018-01-18 15:44:06 +0500

    form

--- views/site/form.php
+++ views/site/form.php
@@ -12,45 +12,8 @@ $this->params['breadcrumbs'][] = $this->title;
                 <div class="box-body">
                     <a href="/site/material"
                        class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'Material Form') ?></h1></a>
-                    <br/>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <a href="/site/all-product"
-                               class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'All Product') ?></h1>
-                            </a>
-                        </div>
-                        <div class="col-md-6">
-                            <a href="/site/product"
-                               class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'One Product') ?></h1>
-                            </a>
-                        </div>
-                    </div>
-                    <br/>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <a href="/site/all-salary"
-                               class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'All Salary') ?></h1></a>
-                        </div>
-                        <div class="col-md-6">
-                            <a href="/site/salary"
-                               class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'One Salary') ?></h1></a>
-                        </div>
-                    </div>
-                    <br/>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <a href="/site/material"
-                               class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'All Client') ?></h1></a>
-                        </div>
-                        <div class="col-md-6">
-                            <a href="/site/material"
-                               class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'One Client') ?></h1></a>
-                        </div>
-                    </div>
-                    <br/>
-                    <a href="/site/material"
-                       class="btn btn-block btn-default btn-lg"><h1><?= Yii::t('app', 'Profit') ?></h1></a>
 
+                    <br/>
                 </div>
             </div>
         </div>
